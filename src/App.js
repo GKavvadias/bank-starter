88d0422015d12620f5e9a97c37bc8bc92e7af064
GKavvadias <il_gato_nero@hotmail.com>
2024-07-03 13:32:22 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
 
-function App() {
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+};
+
+function reducer(state, action) {
+  if (!state.isActive && action.type !== "openAccount") return state;
+
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, balance: 500, isActive: true };
+    case "deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "withdraw":
+      return {
+        ...state,
+        balance:
+          state.balance >= 50 ? state.balance - action.payload : state.balance,
+      };
+    case "requestLoan":
+      return {
+        ...state,
+        loan:
+          state.loan === action.payload
+            ? state.loan
+            : state.loan + action.payload,
+        balance:
+          state.loan === action.payload
+            ? state.balance
+            : state.balance + action.payload,
+      };
+    case "payLoan":
+      return {
+        ...state,
+        balance:
+          state.balance > action.payload
+            ? state.balance - action.payload
+            : state.balance,
+        loan:
+          state.loan === action.payload
+            ? state.loan - action.payload
+            : state.loan,
+      };
+    case "closeAccount":
+      if (state.balance === 0 && state.loan === 0) {
+        return { ...initialState };
+      } else return state;
+    default:
+      throw new Error("💥BAM💥");
+  }
+}
+
+export default function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
+
+      <p>
+        <button
+          onClick={() => dispatch({ type: "openAccount" })}
+          disabled={isActive}
         >
-          Learn React
-        </a>
-      </header>
+          Open account
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "deposit", payload: 150 })}
+          disabled={!isActive}
+        >
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "withdraw", payload: 50 })}
+          disabled={!isActive}
+        >
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "requestLoan", payload: 5000 })}
+          disabled={!isActive}
+        >
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "payLoan", payload: 5000 })}
+          disabled={!isActive}
+        >
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "closeAccount" })}
+          disabled={!isActive}
+        >
+          Close account
+        </button>
+      </p>
     </div>
   );
 }
-
-export default App;
